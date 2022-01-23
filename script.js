75ad5ac47a316339f2a793a672ea7884d77884ef
leovantine <leonardovantine2@gmail.com>
2022-01-23 19:19:40 -0300

    Exercise 1

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Léo"
+let number = 16
+let anoAtual = 2022
+let subtração = 2022 - 16
+ console.log(16 + 2022)
+ console.log("Estou mexendo no console.log")
+
+
+ let quartaFeira = "boolean"
+  console.log(quartaFeira)
